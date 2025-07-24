c98825fc05d5511857f47933d357b19f2fa264db
Lucia-ship-it <lucia.kobzova@icloud.com>
2025-07-24 18:57:18 +0200

    1

--- 01_page_metody/test_page.py
+++ 01_page_metody/test_page.py
@@ -1,3 +1,24 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, sync_playwright
 def test_click(page: Page):
-    
+    # přejít na stránku http://the-internet.herokuapp.com
+    page.goto("http://the-internet.herokuapp.com")
+    # kliknout na tlačítko "Hovers"
+    # #content > ul:nth-child(4) > li:nth-child(25) > a:nth-child(1)
+    # a[href='/hovers']
+
+    link = page.locator("a[href='/hovers']")
+    link.click()
+    # zkontrolovat, že nové url je http://the-internet.herokuapp.com/hovers
+    assert page.url == "http://the-internet.herokuapp.com/hovers"
+
+def test_click_2():
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False, slow_mo=5000)
+        # context = browser.new_context()
+        # page = context.new_page()
+        page = browser.new_page()
+
+        page.goto("http://the-internet.herokuapp.com")
+        link = page.locator("a[href='/hovers']")
+        link.click()
+        assert page.url == "http://the-internet.herokuapp.com/hovers"
---
+++ teoria.md
@@ -0,0 +1,22 @@
+browser = chromium, firefox, webkit
+
+
+
+context =
+
+
+page = 1 zalozka v prehliadaci
+
+
+v praxi sa tie operacne systemy mozu chovat trosku inak
+
+ #vytvorime si svoju vlastnu stranku
+
+ sync_playwright = pridame si do importu
+ launch ma moznost veeela parametrov 
+ headless= True - spusta sa na pozazi a true je defaultne
+
+ ak bude false - budeme vidiet, ako sa to deje a chova sa to viac ako realny uzivatel
+ nevyhoda - ako uzivatel s tym mozem interagovat a klikat tam. coz moze dopadnut spatne
+
+ slow_mo- jednotlive operacie sa robia trosku pomalsie. aby sme to vobec stihli vidiet
